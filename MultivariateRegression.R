4344ba600f636edb0537696ef83b82b9148f19b8
Krithikap019 <42508949+Krithikap019@users.noreply.github.com>
2019-11-19 15:27:33 +0530

    Delete MultivariateRegression.R

--- MultivariateRegression.R
+++
@@ -1,8 +0,0 @@
-data <- read.csv("C:\\Users\\user\\Desktop\\R\\iris.csv")
-summary(data)
-m <- lm(cbind(data$sepal_length,data$sepal_width) ~ data$petal_length + data$petal_width)
-summary(m)
-mm <- manova(m)
-mm
-summary(mm)
-
